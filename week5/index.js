935ad112fadb8742852084790f08f072fed3d7f9
Aditya Sharma <adsharma515@gmail.com>
2025-05-28 03:46:12 +0530

    Updating new file

--- week5/index.js
+++ week5/index.js
@@ -1,31 +1,27 @@
 const express= require("express");
-const axios= require("axios");
+const cors=require("cors")
 const app=express();
+app.use(express.json());
+app.use(cors());
 
-app.get("/multiply",function(req,res){
-    const a=req.query.a;
-    const b=req.query.b;
-    const ans=a*b;
-    res.json({ans});
-})
-
-app.get("/add",function(req,res){
-    const a=parseInt(req.query.a);
-    const b=parseInt(req.query.b);
+app.post("/add",function(req,res){
+    const a=parseInt(req.body.a);
+    const b=parseInt(req.body.b);
     const ans=a+b;
+    console.log(ans);
     res.json({ans});
 })
-app.get("/Divide",function(req,res){
-    const a=req.query.a;
-    const b=req.query.b;
-    const ans=a/b;
-    res.json({ans});
-})
-app.get("/Subtract",function(req,res){
-    const a=req.query.a;
-    const b=req.query.b;
-    const ans=a-b;
-    res.json({ans});
-})
+// app.get("/Divide",function(req,res){
+//     const a=req.query.a;
+//     const b=req.query.b;
+//     const ans=a/b;
+//     res.json({ans});
+// })
+// app.get("/Subtract",function(req,res){
+//     const a=req.query.a;
+//     const b=req.query.b;
+//     const ans=a-b;
+//     res.json({ans});
+// })
 
 app.listen(3000);
